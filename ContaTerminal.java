1543ca2158b22231b7b847bf2fe58863a8d67930
jairzinhou <jairaraujo209@gmail.com>
2023-09-05 17:13:33 -0300

    meu commit

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("digite a agencia da sua conta: ");
+        String agencia=scanner.nextLine();
+
+        System.out.println("digite seu nome: ");
+        String nomeCliente=scanner.nextLine();
+
+        System.out.println("digite o numero da sua conta: ");
+        int numero=scanner.nextInt();
+
+        System.out.println("digite o saldo da sua conta: ");
+        double saldo=scanner.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+",conta "+numero+" e seu saldo "+ saldo+" já está disponível para saque.");
+    }
 }
